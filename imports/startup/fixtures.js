d2d99e823526ea07b0ca761d34b3a1ee392839d1
rtribaldos <rtribaldos@registro-publico.gob.pa>
2016-10-28 10:26:17 -0500

    Step 12 - Search, sort, pagination and reactive vars

--- imports/startup/fixtures.js
+++ imports/startup/fixtures.js
@@ -16,6 +16,42 @@ Meteor.startup(() => {
       'name': 'Savage lounging',
       'description': 'Leisure suit required. And only fiercest manners.',
       'public': true
+    }, {
+      'name': 'Dubstep-Free Zone',
+      'description': 'Fast just got faster with Nexus S.',
+      'public': true
+    }, {
+      'name': 'All dubstep all the time',
+      'description': 'Get it on!',
+      'public': true
+    }, {
+      'name': 'Savage lounging',
+      'description': 'Leisure suit required. And only fiercest manners.',
+      'public': true
+    }, {
+      'name': 'Dubstep-Free Zone',
+      'description': 'Fast just got faster with Nexus S.',
+      'public': true
+    }, {
+      'name': 'All dubstep all the time',
+      'description': 'Get it on!',
+      'public': true
+    }, {
+      'name': 'Savage lounging',
+      'description': 'Leisure suit required. And only fiercest manners.',
+      'public': true
+    }, {
+      'name': 'Dubstep-Free Zone',
+      'description': 'Fast just got faster with Nexus S.',
+      'public': true
+    }, {
+      'name': 'All dubstep all the time',
+      'description': 'Get it on!',
+      'public': true
+    }, {
+      'name': 'Savage lounging',
+      'description': 'Leisure suit required. And only fiercest manners.',
+      'public': true
     }];
 
     parties.forEach((party) => {
---
+++ imports/ui/components/partiesSort/partiesSort.js
@@ -0,0 +1,33 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import template from './partiesSort.html';
+
+class PartiesSort {
+  constructor() {
+    this.changed();
+  }
+
+  changed() {
+    this.onChange({
+      sort: {
+        [this.property]: parseInt(this.order)
+      }
+    });
+  }
+}
+
+const name = 'partiesSort';
+
+export default angular.module(name, [
+    angularMeteor
+]).component(name, {
+  template,
+  bindings: {
+    onChange: '&',
+    property: '@',
+    order: '@'
+  },
+  controllerAs: name,
+  controller: PartiesSort
+});
